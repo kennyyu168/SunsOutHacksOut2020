abb5d90a31a9f8e56b4002edbce394ca275e0fe0
kennyyu168 <kennyyu168@yahoo.com>
2020-08-29 10:40:34 -0700

    changed snackbar

---
+++ SOHO2020/app/src/main/java/com/example/soho2020/NewTaskFragment.java
@@ -0,0 +1,33 @@
+package com.example.soho2020;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewTaskFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewTaskFragment extends Fragment {
+
+    public NewTaskFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_task, container, false);
+    }
+}
